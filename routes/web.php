e11129fb7ab2b53d4aaf20cbc0da9de7c797da58
Veasna <Veasna3d@gmail.com>
2022-07-21 22:45:29 +0700

    Users

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Users\UserIndex;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::get('/users', UserIndex::class)->name('users.index');
 });
